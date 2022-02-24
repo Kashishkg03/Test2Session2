aec23d2c173bf0e7261cc5a3b056be68106169da
Kashish03 <97089600+Kashishkg03@users.noreply.github.com>
2022-02-24 08:51:35 +0000

    program is completed

--- p2final.c
+++ p2final.c
@@ -1,1 +1,31 @@
-
+#include<stdio.h>
+#include<math.h>
+void input_triangle(float *x1,float *y1,float *x2,float *y2,
+float *x3,float *y3)
+{
+  printf("enter the coorinates\n");
+  scanf("%f%f%f%f%f%f",x1,x2,y1,y2,x3,y3);
+}
+int is_triangle(float x1,float y1,float x2,float y2,float x3,float y3)
+{
+  int area;
+  area = 0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+  return area;
+}
+void output(float x1,float y1,float x2,float y2,float x3,float y3, int  istriangle)
+{
+  if(istriangle==0)
+  {
+    printf("the given points doenot form a triangle\n");
+  }
+  else
+    printf("the given points form a triangle\n");
+}
+int main()
+{
+  float x1,y1,x2,y2,x3,y3,result;
+  input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+  is_triangle(x1,y1,x2,y2,x3,y3);
+  output(x1,y1,x2,y2,x3,y3,result);
+  return 0;
+}
